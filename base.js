5aaed1a87ee0ad5041f203cb3ccb24ddf400c60a
Peter Svedman <peter.svedman@gmail.com>
2016-05-19 21:04:39 +0200

    JQuery base fetch
    And some handling functions

--- base.js
+++ base.js
@@ -1,3 +1,26 @@
+/* Chuck Norris sees you */
 console.log("Im working just fine");
-$(document).ready(function(){});
-// boilerplate
+var chuckNorris = "",
+  chuckStatus = "";
+  threeChucked=[];
+// Function threeCut cut up a quote in three suitable parts and returns them as an array of three strings  
+function threeCut(str) {
+	var oneStr="", twoStr="", threeStr="";
+	var one=0, two=0, three=0;
+	one = str.indexOf(' ',(str.length/5)*1)
+	two = str.indexOf(' ',(str.length/5)*2);
+	three = str.indexOf((' ', str.length/5)*3);
+	//console.log (one, two, three, str.slice(0,one), str.slice(one,two), str.slice(two, str.length));
+	return [str.slice(0,one), str.slice(one,two), str.slice(two, str.length)];
+}  
+  
+$(document).ready(function() {
+  $("button").click(function() {
+    $.getJSON("http://api.icndb.com/jokes/random", function(chuckNorris, chuckStatus) {
+      $("#test").text(chuckNorris.value.joke);
+      console.log(chuckStatus);
+	  threeChucked=threeCut(chuckNorris.value.joke);
+	  console.log(threeChucked);
+    });
+  });
+});
