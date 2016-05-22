09091f0115ace481dac706ecfd495e6c63adb659
Peter Svedman <peter.svedman@gmail.com>
2016-05-22 15:34:36 +0200

    Tweets are working!

--- base.js
+++ base.js
@@ -1,8 +1,8 @@
 /* Chuck Norris sees you */
 console.log("Im working just fine");
-var chuckNorris = "",
-  chuckStatus = "";
-threeChucked = "";
+var chuckNorris = "";
+var chuckStatus = "";
+var threeChucked = "";
 
 // Function fixQuote fixes the &quote problem
 function fixQuote(stru) {
@@ -10,14 +10,26 @@ function fixQuote(stru) {
 }
 // main script section
 $(document).ready(function() {
-  $("button").click(function() {
-    $.getJSON("http://api.icndb.com/jokes/random", function(chuckNorris, chuckStatus) {
-      //$("#test").text(chuckNorris.value.joke);
-      console.log(chuckStatus);
-      //threeChucked = threeCut(fixQuote(chuckNorris.value.joke));
-      //console.log(threeChucked);
-      threeChucked=fixQuote(chuckNorris.value.joke);
-      $("#quote-elem").text(threeChucked);
-    });
-  });
-});
+	// Load a joke from the API
+        $.getJSON("http://api.icndb.com/jokes/random", function(chuckNorris, chuckStatus) {
+        console.log(chuckStatus);
+        threeChucked = fixQuote(chuckNorris.value.joke);
+        $("#quote-elem").text(threeChucked);
+		 });
+	  
+	// On new-button click load a new joke  
+	  $("#new-button").click(function() {
+        $.getJSON("http://api.icndb.com/jokes/random", function(chuckNorris, chuckStatus) {
+          console.log(chuckStatus);
+          threeChucked = fixQuote(chuckNorris.value.joke);
+          $("#quote-elem").text(threeChucked);
+        });
+      });
+	  // On tweet.button click load tweet page
+      $("button.tweet-button").click(function(e) {
+         e.preventDefault();
+		 var loc = threeChucked;
+        //We trigger a new window with the Twitter dialog, in the middle of the page
+         window.open('http://twitter.com/share?url=' + loc + '&text=' + threeChucked + '&', 'twitterwindow', 'height=450, width=550, top=' + ($(window).height() / 2 - 225) + ', left=' + $(window).width() / 2 + ', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+        });
+      });
